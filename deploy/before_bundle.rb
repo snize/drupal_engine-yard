2cb2108a408c9ddca69218454478aa2a117cd9b4
Tomotsugu Kaneko <snizee@gmail.com>
2015-04-08 17:58:02 +0900

    remove submodule

---
+++ deploy/before_bundle.rb
@@ -0,0 +1,10 @@
+# link sites directory
+if !Dir.exist?(config.shared_path + "/sites") then
+  run "cp -R #{config.release_path}/sites #{config.shared_path}/sites"
+end
+run "rm -Rf #{config.release_path}/sites && ln -s #{config.shared_path}/sites ./sites"
+
+# set timezon in php.ini
+#sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cgi-php5.4/ext-active/timezone.ini"
+#sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cli-php5.4/ext-active/timezone.ini"
+#sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/fpm-php5.4/ext-active/timezone.ini"
